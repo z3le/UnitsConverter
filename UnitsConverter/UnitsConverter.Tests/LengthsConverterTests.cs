a790ee3b284f61579ca872a0895a74e92f1dc79b
Rossi Yordanova <riordanova@gmail.com>
2015-09-23 21:09:27 +0300

    Adding Tests

---
+++ UnitsConverter/UnitsConverter.Tests/LengthsConverterTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitsConverter.Tests
+{
+    [TestClass]
+    public class LengthsConverterTests
+    {
+        [TestMethod]
+        public void LenghtsConverter_ConvertCentimetersToMeters_Succes()
+        {
+            decimal centimeters = 20;
+            decimal meters = LengthsConverter.ConvertCentimetersToMeters(centimeters);
+
+            Assert.AreEqual(meters, 0.2M);
+        }
+
+        [TestMethod]
+        public void LenghtsConverter_ConvertKilometersToMiles_Succes()
+        {
+            decimal kilometers = 15M;
+            decimal meters = LengthsConverter.ConvertKilometersToMiles(kilometers);
+
+            Assert.AreEqual(meters, 9.320565M);
+        }
+    }
+}
