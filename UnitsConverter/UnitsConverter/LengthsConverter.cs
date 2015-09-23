f03210a6e1ef8c0d64abe95f185034e794cef36f
Rossi Yordanova <riordanova@gmail.com>
2015-09-23 21:12:04 +0300

    change in file names

---
+++ UnitsConverter/UnitsConverter/LengthsConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitsConverter
+{
+    /// <summary>
+    /// Lenghts converter class
+    /// </summary>
+    public class LengthsConverter
+    {
+        /// <summary>
+        /// Converts the centimeters to meters.
+        /// </summary>
+        /// <param name="centimeters">The centimeters.</param>
+        /// <returns></returns>
+        public static decimal ConvertCentimetersToMeters(decimal centimeters)
+        {
+            return centimeters / 100M;
+        }
+
+        /// <summary>
+        /// Converts kilometers to miles.
+        /// </summary>
+        /// <param name="kilometers"> kilometers.</param>
+        /// <returns>miles</returns>
+        public static decimal ConvertKilometersToMiles(decimal kilometers)
+        {
+            return kilometers * 0.621371M;
+        }
+    }
+}
